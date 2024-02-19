d51f78df177eca925e33898d3471acfa10209996
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2024-02-19 20:04:57 +0200

    Support for dynamic (relocatable) AlitaUI configuration

--- routes/new_ui.py
+++ routes/new_ui.py
@@ -3,7 +3,7 @@ import flask
 from pathlib import Path
 
 from pylon.core.tools import web, log
-from tools import theme
+from tools import theme, VaultClient
 
 from werkzeug.exceptions import NotFound
 
@@ -21,12 +21,14 @@ class Route:
                 sub_path, base_path.joinpath('index.html')
             )
             #
+            secrets = VaultClient().get_all_secrets()
+            #
             vite_server_url = flask.url_for(
                 "api.v1.alita_ui.ui_ci", _external=True
             ).replace("api/v1/alita_ui/ui_ci/", "api/v1")
             #
             vite_base_uri = flask.url_for("alita_ui.route_alita_ui").rstrip("/")
-            vite_public_project_id = 1
+            vite_public_project_id = int(secrets.get("ai_project_id", "1"))
             #
             alita_ui_config = json.dumps({
                 "vite_server_url": vite_server_url,
@@ -40,11 +42,11 @@ class Route:
                 idx_data = idx_file.read()
             #
             idx_data = idx_data.replace(
-                '<!-- alita_ui_config -->',
-                f"<script>window.alita_ui_config = JSON.parse('{alita_ui_config}');</script>"
+                'src="./assets', f'src="{vite_base_uri}/assets'
             )
             idx_data = idx_data.replace(
-                'src="./assets', f'src="{vite_base_uri}/assets'
+                '<!-- alita_ui_config -->',
+                f"<script>window.alita_ui_config = JSON.parse('{alita_ui_config}');</script>"
             )
             #
             response = flask.make_response(idx_data, 200)
